4e3f4e8780b10614fa3533f4259467b9ca20855c
goodboyvincenzo@gmail.com <goodboyvincenzo@gmail.com>
2024-01-24 11:26:45 +0000

    updated int models

--- models/intermediate/int_sales__margin.sql
+++ models/intermediate/int_sales__margin.sql
@@ -4,7 +4,7 @@ select
     orders_id,
     revenue,
     quantity,
-    cast(purchase_price as float64) as purchase_price,
+    cast(purchase_price as float64),
     round(s.quantity * cast(p.purchase_price as float64), 2) as purchase_cost,
     s.revenue - round(s.quantity * cast(p.purchase_price as float64), 2) as margin
 from {{ ref("stg_raw__sales") }} s
